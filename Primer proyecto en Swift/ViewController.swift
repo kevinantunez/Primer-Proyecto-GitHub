c163d34c1069ae629ae5fd693781ae251d0afc94
KevinPages <KevinPages@kevinpagess-mac.local>
2015-12-01 18:04:04 -0600

    primera modificación , ingrese un comentario

--- Primer proyecto en Swift/ViewController.swift
+++ Primer proyecto en Swift/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //ESTE ES UN COMENTARIO PARA VERIFICART QUE FUNCIONA NUESTRO PROYECTO CON GITHUB
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
